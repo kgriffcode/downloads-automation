b93be053091f7f1240f7a23ef827da9b754c0247
griffithdesigned <griffithdesigned@gmail.com>
2022-12-27 17:21:29 -0600

    implemented funneling for images. close #3

--- file_automation.py
+++ file_automation.py
@@ -14,11 +14,20 @@ while True:
 
     # Declare file destination
     pdf_folder = os.path.expanduser('~/Documents/PDFs')
-    imgs_folder = os.path.expanderuser('~/Documents/Images')
+    imgs_folder = os.path.expanduser('~/Documents/Images')
+    img_types = ['.jpg', '.jpeg', '.png', '.webp']
 
+    # PDF Funneling
     for file in files:
         if file.endswith('.pdf'):
             file_path = os.path.join(downloads_folder, file).lower()
             shutil.move(file_path, pdf_folder)
+
+    # Image funneling .jpg, .jpeg, .png, .webp
+    for file in files:
+        if file.endswith((".jpg", ".jpeg", ".png", ".webp")):
+            file_path = os.path.join(downloads_folder, file).lower()
+            shutil.move(file_path, imgs_folder)
+
 
     time.sleep(60)
