7cf1b436ce70fd02fe93514ceff9fc8dcf78c184
griffithdesigned <griffithdesigned@gmail.com>
2022-12-27 16:57:37 -0600

    added an infinite while loop that checks ~/Downloads/ every 60 seconds & made our script lowercase all files being moved. close #1, close #2

--- file_automation.py
+++ file_automation.py
@@ -1,12 +1,24 @@
 # Bring in modules
+import time
 import os
 import shutil
 
 # Declare path
 downloads_folder = os.path.expanduser('~/Downloads')
 
-# List the files in ~/Downloads
-files = os.listdir(downloads_folder)
+# Create infinite loop
+while True:
 
-# Declare file destination
-pdf_folder = os.path.expanduser('~/Documents/PDFs')
+    # List the files in ~/Downloads
+    files = os.listdir(downloads_folder)
+
+    # Declare file destination
+    pdf_folder = os.path.expanduser('~/Documents/PDFs')
+    imgs_folder = os.path.expanderuser('~/Documents/Images')
+
+    for file in files:
+        if file.endswith('.pdf'):
+            file_path = os.path.join(downloads_folder, file).lower()
+            shutil.move(file_path, pdf_folder)
+
+    time.sleep(60)
